d55edc49d9678996fec6ef6d2c0898615061f238
Gnutrients <copswatch@protonmail.com>
2023-07-23 15:03:24 -0400

    Adding types.ts

---
+++ src/types.ts
@@ -0,0 +1,30 @@
+/**
+ * Type for a location in the application
+ */
+type Location = {
+    latitude : number,
+    longitude : number
+}
+
+/**
+ * A singular stop on the walking tour is types to have this
+ * information
+ */
+type WalkingTourStop = {
+    id : string,
+    name : String,
+    location : Location,
+    content_file : string
+}
+
+/**
+ * Specifies domain specific information about the screens used in the 
+ * application. 
+ */
+type RootStackParamList = {
+    Main : undefined,
+    Map : {
+        location : Location
+    },
+    Detail : undefined
+};
