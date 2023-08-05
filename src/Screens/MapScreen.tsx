c8f91d806a3c6844110639799b28287cc13b114a
Gnutrients <copswatch@protonmail.com>
2023-08-05 03:27:31 -0400

    Modifying MapScreen.tsx
    Averaging all markers to get an original position from the given marker

--- src/Screens/MapScreen.tsx
+++ src/Screens/MapScreen.tsx
@@ -1,24 +1,40 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { StyleSheet } from "react-native";
-import MapView, {UrlTile} from "react-native-maps";
+import MapView, {MapMarker, UrlTile} from "react-native-maps";
 type Props = NativeStackScreenProps<RootStackParamList, 'Map'>;
 
 const Styles = StyleSheet.create({
   mapStyle: {
-    minHeight: 500
+    minHeight: 500,
+    ...StyleSheet.absoluteFillObject
   }
 })
-const MapScreen = ({ navigation }: Props): React.ReactElement => {
+const MapScreen = ({ route, navigation }: Props): React.ReactElement => {
+  const { marker_locations } = route.params;
+  const marker_latitudes = marker_locations.map((location) => location.latitude);
+  const marker_longitudes = marker_locations.map((location) => location.longitude);
+  const [
+    min_latitude,
+    min_longitude,
+    max_latitude,
+    max_longitude
+  ] = [Math.min(...marker_latitudes), Math.min(...marker_longitudes), Math.max(...marker_latitudes), Math.max(...marker_longitudes)]
+  
+  const midpoint_latitude = (min_latitude + max_latitude) / 2;
+  const midpoint_longitude = (min_longitude + max_longitude) / 2;
+  
   return (
     <MapView
       style={Styles.mapStyle}
       initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
+        latitude: midpoint_latitude,
+        longitude: midpoint_longitude,
+        latitudeDelta: 0.2,
         longitudeDelta: 0.0421,
       }}>
-    
+        {marker_locations.map((location, index) => {
+          return <MapMarker key={index + 1} coordinate={location}/>
+        })}
     </MapView>
   )
 }
