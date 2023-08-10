57fb19914c0bae71a388c0e558d4b3c8f26ec317
Gnutrients <copswatch@protonmail.com>
2023-08-10 00:31:04 -0400

    Adding colors.ts

---
+++ src/colors.ts
@@ -0,0 +1,17 @@
+/**
+ * Colors used for styles in the application
+ */
+const Colors = {
+    light : {
+        background : '#431100',
+        card : '#1A0700',
+        pressed : '#AC3006'
+    },
+    dark : {
+        background : '#431100',
+        card : '#1A0700',
+        pressed : '#AC3006'
+    }
+}
+
+export default Colors;
