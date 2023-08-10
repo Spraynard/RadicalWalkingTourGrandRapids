e8aac5c1ffb5027866634a508d568406d5217b7e
Gnutrients <copswatch@protonmail.com>
2023-08-10 00:33:06 -0400

    Modifying App.tsx
    Removing unnecessary items
    
    Adding LightTheme and DarkTheme

--- App.tsx
+++ App.tsx
@@ -6,74 +6,67 @@
  */
 
 import React, { createContext, useState } from 'react';
-import type { PropsWithChildren } from 'react';
 import {
-  Button,
-  StyleSheet,
+    Button, useColorScheme,
 } from 'react-native';
 
 import MainScreen from './src/Screens/MainScreen';
 import { NativeStackNavigationProp, createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, DarkTheme, NavigationContainer } from '@react-navigation/native';
 import MapScreen from './src/Screens/MapScreen';
 import DetailScreen from './src/Screens/DetailScreen';
 import walking_tour_stops from "./src/walking_tour_stops.json";
+import Colors from './src/colors';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-function App(): JSX.Element {
-  const marker_locations : Location[] = walking_tour_stops.map(( item : WalkingTourStop) => {
-    return item.location;
-  });
-  return (
-    <NavigationContainer>
-        <Stack.Navigator initialRouteName='Main'>
-          <Stack.Screen
-            name="Detail"
-            component={DetailScreen}
-            // THis shows name of the stop on the bar
-            options={({route}) => ({
-              title : route.params.walking_tour_stop.name
-            })}
-          />
-          <Stack.Screen
-            name="Main"
-            component={MainScreen}
-            options={({navigation} : { navigation : NativeStackNavigationProp<RootStackParamList, "Map"> }) => ({
-              headerRight : () => (
-                <Button title="Map View" onPress={() => navigation.navigate("Map", {
-                  marker_locations
-                })} />
-              )
-            })}
-          />
-          <Stack.Screen
-            name="Map"
-            component={MapScreen}
-          />
-        </Stack.Navigator>
-    </NavigationContainer>
-  );
+// Light Theme
+const RadicalTheme = {
+    ...DefaultTheme,
+
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+// Dark Theme
+const RadicalDarkTheme = {
+    ...DarkTheme,
+    colors : {
+        ...DarkTheme.colors,
+        background : Colors.dark.background,
+        card : Colors.dark.card,
+    }
+}
 
-});
+function App(): JSX.Element {
+    const isDarkMode = useColorScheme() === 'dark';
+
+    return (
+        <NavigationContainer theme={isDarkMode ? RadicalDarkTheme : RadicalTheme}>
+            <Stack.Navigator initialRouteName='Main'>
+                <Stack.Screen
+                    name="Detail"
+                    component={DetailScreen}
+                    options={({ route }) => ({
+                        title: route.params.walking_tour_stop.name
+                    })}
+                />
+                <Stack.Screen
+                    name="Main"
+                    component={MainScreen}
+                    options={({ navigation }: { navigation: NativeStackNavigationProp<RootStackParamList, "Map"> }) => ({
+                        headerRight: () => (
+                            <Button title="Map View" onPress={() => navigation.navigate("Map", {
+                                walking_tour_stops
+                            })} />
+                        )
+                    })}
+                />
+                <Stack.Screen
+                    name="Map"
+                    component={MapScreen}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
 
 export default App;
