2d50288d51c387e87b7bc8255ad5c939be681a40
Gnutrients <copswatch@protonmail.com>
2023-08-10 00:39:21 -0400

    Modifying RadicalScreen.tsx
    Modifying styles here so basically modifying styles for the whole application

--- src/RadicalComponents/RadicalScreen.tsx
+++ src/RadicalComponents/RadicalScreen.tsx
@@ -2,17 +2,13 @@
  * Main screen component that includes base styles and maybe even some functionality
  */
 
-import { View, SafeAreaView, useColorScheme, } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import { SafeAreaView, useColorScheme } from "react-native";
+import Colors from "../colors";
 
 type Props = React.PropsWithChildren;
 
 const RadicalScreen = ({ children, ...props }: Props) => {
-    const isDarkMode = useColorScheme() === 'dark';
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
-    return (<SafeAreaView style={[backgroundStyle, { height : "100%" }]}>
+    return (<SafeAreaView style={{ height : "100%" }}>
         {children}
     </SafeAreaView>)
 }
