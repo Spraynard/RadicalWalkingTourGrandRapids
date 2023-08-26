a209b69960495e90b2caeee8d43153cebc07385a
Gnutrients <copswatch@protonmail.com>
2023-08-26 18:07:13 -0400

    Adding constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+const DEFAULT_IMAGE = require("./static/images/default_image_not_found.jpg");
+const ANDROID_IMAGE_ASSET_BASE = "asset:/images/walking_tour_stops/";
+const IOS_IMAGE_ASSET_BASE = "";
+
+export default ({
+    DEFAULT_IMAGE,
+    ANDROID_IMAGE_ASSET_BASE,
+    IOS_IMAGE_ASSET_BASE
+})
