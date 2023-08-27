079822daca97f11916ce19a861e4488539488d01
Gnutrients <copswatch@protonmail.com>
2023-08-26 21:38:39 -0400

    Modifying constants.ts
    Adding FONT_SIZE_BASE

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,11 @@
 const DEFAULT_IMAGE = require("./static/images/default_image_not_found.jpg");
 const ANDROID_IMAGE_ASSET_BASE = "asset:/images/walking_tour_stops/";
 const IOS_IMAGE_ASSET_BASE = "";
+const FONT_SIZE_BASE = 25;
 
 export default ({
     DEFAULT_IMAGE,
     ANDROID_IMAGE_ASSET_BASE,
-    IOS_IMAGE_ASSET_BASE
+    IOS_IMAGE_ASSET_BASE,
+    FONT_SIZE_BASE
 })
